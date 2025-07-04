c7260342f1da20a4d7770817b0313e9138155627
kotharihimanshi <himanshikothari9@gmail.com>
2025-07-04 22:56:47 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -86,7 +86,7 @@ const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV=="production"){
-  app,use(express.static(path.join(__dirname , "../frontend/dist")))
+  app.use(express.static(path.join(__dirname , "../frontend/dist")))
 
 
   app.get("*", (req, res) => {
